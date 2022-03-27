81ff583f1b1650fa2efbce6f1e06df4da4d8a98e
Max Malm <benjick@dumfan.net>
2022-03-27 14:12:28 +0200

    Add logo

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -135,7 +135,7 @@ export const Layout: React.FC = ({ children }) => {
         </Transition.Root>
 
         <div className="flex flex-col flex-1">
-          <div className="sticky top-0 z-10 pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
+          <div className="sticky top-0 z-10 pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100 flex">
             <button
               type="button"
               className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
@@ -144,6 +144,15 @@ export const Layout: React.FC = ({ children }) => {
               <span className="sr-only">Open sidebar</span>
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </button>
+
+            <div className="h-12 w-full">
+              <Image
+                src={homeflix}
+                height="48px"
+                objectFit="contain"
+                alt="Homeflix"
+              />
+            </div>
           </div>
           <main className="flex-1" {...handlers}>
             <div className="py-6">
